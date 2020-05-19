a98a489e59d7c0c997f7b77e6cb46d6bad31b56d
rafal <rafalplaszczyk.com@gmail.com>
2020-05-19 23:57:19 +0200

    Update Wherefore art thou.js
    Refactor

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Wherefore art thou.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Wherefore art thou.js
@@ -1,16 +1,13 @@
 function whatIsInAName(collection, source) {
   var arr = [];
   // Only change code below this line
-  console.log(Object.entries(source))
   Object.values(collection).forEach(x => {
-    console.log(Object.entries(source).every(y => {
+    if (Object.entries(source).every(y => {
       return x.hasOwnProperty(y[0]) && x[y[0]] === y[1]
-    }))
-    if(Object.entries(source).every(y => {
-      return x.hasOwnProperty(y[0]) && x[y[0]] === y[1]
-    })) arr.push(x)
+    })) {
+      arr.push(x)
+    }
   })
-
   // Only change code above this line
   return arr;
 }
