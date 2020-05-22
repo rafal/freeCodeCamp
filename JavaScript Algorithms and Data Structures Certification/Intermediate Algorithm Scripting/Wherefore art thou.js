6f6bd58d990604b975abe673e57ff6a1f048ec70
rafal <rafalplaszczyk.com@gmail.com>
2020-05-23 00:17:51 +0200

    Update Wherefore art thou.js

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Wherefore art thou.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Wherefore art thou.js
@@ -1,11 +1,7 @@
 function whatIsInAName(collection, source) {
-  var arr = [];
   // Only change code below this line
-  Object.values(collection).forEach(x => {
-    if (Object.entries(source).every(y => x[y[0]] === y[1])) arr.push(x)
-  })
+  return collection.filter(x => Object.entries(source).every(y => x[y[0]] === y[1]))
   // Only change code above this line
-  return arr;
 }
 
 whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 }) 
