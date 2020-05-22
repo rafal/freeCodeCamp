51dbace605e8d84b2345c4f9da9efb80f9e2bcdb
rafal <rafalplaszczyk.com@gmail.com>
2020-05-22 22:34:44 +0200

    Update Nesting For Loops - Recursive.js

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Nesting For Loops - Recursive.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/Nesting For Loops - Recursive.js
@@ -20,4 +20,37 @@ function multiplyAll(arr) {
   }
 }
 
-console.log(multiplyAll([[1,2],[3,4],[5,6,7]]))
+const simpleMulti = arr => {
+  return arr.length <= 1
+    ? arr[0]
+    : arr[0] * simpleMulti(arr.slice(1))
+}
+console.log(simpleMulti([1,2,3,4,5]))
+
+const multi = arr => {
+  return arr.length <= 1
+    ? Array.isArray(arr[0])
+      ? multi(arr[0])
+      : arr[0]
+    : Array.isArray(arr[0])
+      ? multi(arr[0]) * multi(arr.slice(1))
+      : arr[0] * multi(arr.slice(1))
+}
+
+const multi2 = arr => {
+  return Array.isArray(arr[0])
+    ? arr.length <= 1
+      ? multi2(arr[0])
+      : multi2(arr[0]) * multi2(arr.slice(1))
+    : arr.length <= 1
+      ? arr[0]
+      : arr[0] * multi2(arr.slice(1))
+}
+const multi3 = arr => arr.reduce((x, y) => {
+  return x *= Array.isArray(y)
+    ? multi3(y)
+    : y
+}, 1)
+console.log(multi3([[],1,2,[3]]))
+
+console.log(multi([[1,2],[3,4],[5,6,7,[2, [2]]]]));
