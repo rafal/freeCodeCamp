19c5da2437f78c2f62515e5a94d1aa0420a5f2fd
rafal <rafalplaszczyk.com@gmail.com>
2020-05-25 23:41:15 +0200

    Create Truncate a String.js

---
+++ JavaScript Algorithms and Data Structures Certification/Basic Algorithm Scripting/Truncate a String.js
@@ -0,0 +1,6 @@
+function truncateString(str, num) {
+  return str.length <= num ? str : str.slice(0, num) + "...";
+}
+
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8))
