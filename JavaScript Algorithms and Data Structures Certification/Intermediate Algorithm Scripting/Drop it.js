a0fb926bbcef4c7952c71149a19dd3d3302b431b
rafal <rafalplaszczyk.com@gmail.com>
2020-05-31 01:40:37 +0200

    Update Drop it.js

--- JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Drop it.js
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/Drop it.js
@@ -1,13 +1,6 @@
 function dropElements(arr, func) {
-  let newArr = [...arr]
-  for(let i =0 ; i < arr.length; i++){
-    if (func(arr[i])) {
-      return arr.slice(i)
-    }else{
-      newArr.splice((newArr.indexOf(arr[i])), 1)
-    }
-  }
-  return newArr;
+  return arr.find(func) ? arr.slice(arr.findIndex(func)) : []
+  // return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func) : arr.length);
 }
 
 dropElements([1, 2, 3, 4], function(n) {return n > 5;})
