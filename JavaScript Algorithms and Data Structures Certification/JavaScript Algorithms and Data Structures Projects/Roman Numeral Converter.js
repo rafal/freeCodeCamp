d78b9627d58e73e87f9025815612a4490596a0b2
rafal <rafalplaszczyk.com@gmail.com>
2020-06-06 19:20:27 +0200

    Update Roman Numeral Converter.js

--- JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
+++ JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Roman Numeral Converter.js
@@ -1,37 +1,18 @@
 function convertToRoman(num) {
-  const romans = {
-    1: "I",
-    2: "II",
-    3: "III",
-    5: "V",
-    10: "X",
-    50: "L",
-    100: "C",
-    500: "D",
-    1000: "M"
-  }
-  let basis;
-  if (num <= 3){
-    basis = "I"
-  } else if (num <= 8) {
-    basis = "V"
-  } else if (num <= 39){
-    basis = "X"
-  } else if (num <= 89){
-    basis = "L"
-  } else if (num <= 399){
-    basis = "C"
-  } else if (num <= 899){
-    basis = "D"
-  } else if (num <= 3999){
-    basis = "M"
-  }
-  console.log(basis)
-  let result = ""
-  if((""+num)[0] <= 3){
-    console.log(basis.repeat((""+num)[0]))
-  }
-  return num;
+ return "I"
+  .repeat(num)
+  .replace(/IIIII/g, "V")
+  .replace(/VV/g, "X")
+  .replace(/XXXXX/g, "L")
+  .replace(/LL/g, "C")
+  .replace(/CCCCC/g, "D")
+  .replace(/DD/g, "M")
+  .replace("IIII", "IV")
+  .replace("XXXX", "XL")
+  .replace("CCCC", "CD")
+  .replace(/VIV/g, "IX")
+  .replace(/LXL/g, "XC")
+  .replace(/DCD/g, "CM")
 }
 
-convertToRoman(36);
+convertToRoman(99);
