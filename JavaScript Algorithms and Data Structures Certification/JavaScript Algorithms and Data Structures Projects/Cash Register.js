f3d764f2c806dc8c01fedf644b21761e6f0a2fcf
rafal <rafalplaszczyk.com@gmail.com>
2020-06-14 23:52:44 +0200

    Update Cash Register.js

--- JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Cash Register.js
+++ JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Cash Register.js
@@ -1,12 +1,13 @@
 function checkCashRegister(price, cash, cid) {
   const priceInCents = price * 100
   const cashInCents = cash * 100
+  const cidInCents = cid.map(x => [x[0], Math.round(x[1] * 100)])
   let change = cashInCents - priceInCents
   const totalCashInDrawer = cid.reduce((x, y) => x + y[1] * 100, 0)
   if (totalCashInDrawer < change) return {status: "INSUFFICIENT_FUNDS", change: []}
-  if (totalCashInDrawer === change) return {status: "CLOSED", change: [...cid]}
+  if (totalCashInDrawer === change) return {status: "CLOSED", change: cid}
 
-  const lookup = {
+  const centValue = {
     PENNY: 1,
     NICKEL: 5,
     DIME: 10,
@@ -17,19 +18,14 @@ function checkCashRegister(price, cash, cid) {
     TWENTY: 2000,
     'ONE HUNDRED': 10000
   }
-
-  cid.map(x => x[1] = x[1] / lookup[x[0]] * 100)
-
-
+  
   let coins = []
-  for (let i = cid.findIndex(x => lookup[x[0]] > change) - 1; i >= 0, change >= 0; i--){
+  for (let i = cidInCents.findIndex(coin => centValue[coin[0]] > change) - 1; i > 0, change > 0; i--){
     if (i < 0) break
-    if (change === 0) break
-    while(lookup[cid[i][0]] <= change && cid[i][1] > 0 && change >= 0){
-      coins.push(cid[i][0])
-      change -= lookup[cid[i][0]]
-      --cid[i][1]
-      if (change === 0) break
+    while(centValue[cidInCents[i][0]] <= change && cidInCents[i][1] / centValue[cid[i][0]]){
+      coins.push(cidInCents[i][0])
+      change -= centValue[cidInCents[i][0]]
+      cidInCents[i][1] -= centValue[cidInCents[i][0]]
     }
   }
   if (change !== 0) return {status: "INSUFFICIENT_FUNDS", change: []}
@@ -38,13 +34,12 @@ function checkCashRegister(price, cash, cid) {
     // if (x.length > 0) console.log(x[x.length - 1][0])
     if (x.length > 0 && x[x.length - 1][0] === y) {
       // console.log(x[x.length - 1][0] +"===" +y)
-      x[x.length - 1] = [y, x[x.length - 1][1] + lookup[y] / 100]
+      x[x.length - 1] = [y, x[x.length - 1][1] + centValue[y] / 100]
       return x
     } else {
-      return x.concat([[y, lookup[y] / 100]])
+      return x.concat([[y, centValue[y] / 100]])
     }
   }, [])
-  console.log(changeInCoinsAndBills)
   return {status: "OPEN", change: changeInCoinsAndBills}
 }
 
