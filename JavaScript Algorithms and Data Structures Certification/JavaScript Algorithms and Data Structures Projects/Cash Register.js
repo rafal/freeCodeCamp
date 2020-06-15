a18e0cc687473a97dd1f039aec1bf32e1ef3f05d
rafal <rafalplaszczyk.com@gmail.com>
2020-06-15 23:30:28 +0200

    Update Cash Register.js

--- JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Cash Register.js
+++ JavaScript Algorithms and Data Structures Certification/JavaScript Algorithms and Data Structures Projects/Cash Register.js
@@ -19,8 +19,9 @@ function checkCashRegister(price, cash, cid) {
     'ONE HUNDRED': 10000
   }
 
+  console.log(cidInCents)
   let coins = []
-  for (let i = cidInCents.findIndex(coin => centValue[coin[0]] > change) - 1; i > 0, change > 0; i--){
+  for (let i = cidInCents.findIndex(coin => centValue[coin[0]] > change) - 1; change > 0; i--){
     if (i < 0) break
     const coin = cidInCents[i][0]
     while(centValue[coin] <= change && cidInCents[i][1] / centValue[cid[i][0]]){
@@ -30,18 +31,35 @@ function checkCashRegister(price, cash, cid) {
     }
   }
   if (change !== 0) return {status: "INSUFFICIENT_FUNDS", change: []}
+  console.log(coins)
 
+  // const changeInCoinsAndBills = coins.reduce((x, y) => {
+  //   // if (x.length > 0) console.log(x[x.length - 1][0])
+  //   if (x.length > 0 && x[x.length - 1][0] === y) {
+  //     // console.log(x[x.length - 1][0] +"===" +y)
+  //     x[x.length - 1] = [y, x[x.length - 1][1] + centValue[y] / 100]
+  //     return x
+  //   } else {
+  //     return x.concat([[y, centValue[y] / 100]])
+  //   }
+  // }, [])
+  // const changeInCoinsAndBills = coins.reduce((x, y) => {
+  //   if (!x.includes(y)) {
+  //     x.push(y)
+  //   }
+  //   return x
+  // }, [])
   const changeInCoinsAndBills = coins.reduce((x, y) => {
-    // if (x.length > 0) console.log(x[x.length - 1][0])
-    if (x.length > 0 && x[x.length - 1][0] === y) {
-      // console.log(x[x.length - 1][0] +"===" +y)
-      x[x.length - 1] = [y, x[x.length - 1][1] + centValue[y] / 100]
-      return x
+    console.log(x.find(z => z[0] === y))
+    if (!(x.find(z => z[0] === y))){
+      x.push([y, centValue[y] / 100])
     } else {
-      return x.concat([[y, centValue[y] / 100]])
+      x[x.findIndex(z => z[0] === y)] = [y, x[x.findIndex(z => z[0] === y)][1] + centValue[y] / 100]
     }
+    return x
   }, [])
+  console.log(changeInCoinsAndBills)
   return {status: "OPEN", change: changeInCoinsAndBills}
 }
 
-checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) 
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])
