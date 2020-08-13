9a318bbba47ff2c02bc9db88516d90e7deac1ebe
rafal <rafalplaszczyk.com@gmail.com>
2020-08-13 12:15:38 +0200

    Create Use Default Props.jsx

---
+++ Front End Libraries Certification/React/Use Default Props.jsx
@@ -0,0 +1,9 @@
+const ShoppingCart = (props) => {
+  ShoppingCart.defaultProps = { items: 0}
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  )
+};
+// change code below this line
