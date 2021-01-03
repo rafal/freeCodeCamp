0d0fe2fb77bf42e59187c8ed65406d57dc38c247
rafal <rafalplaszczyk.com@gmail.com>
2021-01-03 04:19:20 +0100

    Create Create a Redux Store.jsx

---
+++ Front End Libraries Certification/Redux/Create a Redux Store.jsx
@@ -0,0 +1,5 @@
+const reducer = (state = 5) => state
+// Redux methods are available from a Redux object
+// For example: Redux.createStore()
+// Define the store here:
+const store = Redux.createStore(redux)
